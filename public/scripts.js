11300b0e4cc61dff38c8d0172693fad284ab600a
Douglas Botello <bdougl5@vt.edu>
2018-04-06 13:44:28 -0400

    Added a data UI. Need to have it display actual data

---
+++ public/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#poison-ivy-link").click(function() {
+    	window.location.href="index.html";
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
